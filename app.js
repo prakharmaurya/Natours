226f40fc09c56e7deb1bf528fc4d40ed53b3d1bc
Excel <000.excel@gmail.com>
2020-04-04 15:23:20 +0530

    Edit a Express App Comment

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 
+//Start express app
 const app = express();
 
 app.set('view engine', 'pug');
